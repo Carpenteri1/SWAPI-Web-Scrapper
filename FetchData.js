48fb91c73bdd84aa338a0793e0f586f78b1e69e6
Carpenteri1 <36822443+Carpenteri1@users.noreply.github.com>
2020-05-15 16:11:59 +0200

    added auto add favorites, fixed bugg add multiple of same character to fave

--- FetchData.js
+++ FetchData.js
@@ -1,10 +1,12 @@
  
 
   const url = 'https://swapi.dev/api/people/?search=';
+  const addToFaveUrl = 'https://swapi.dev/api/people/?page=';
   let userInput = undefined;
-
-  window.addEventListener('load', () => { 
-  MainFunction();
+  let selectedFaves = ['Luke Skywalker','Yoda','Darth Vader','C-3PO','R2D2','Han Solo'];
+  window.addEventListener('load', () => {
+    fetchingDataToAutoAdd(); 
+    MainFunction();
 })
 
 
@@ -34,6 +36,27 @@ function targetInnerText()
       })
 }
 
+async function fetchingDataToAutoAdd()
+{
+  const options = 
+    {
+      method: 'GET'
+    }
+    for(pageNum = 1;pageNum<=9;pageNum++)
+    {
+        let listOfFaves = document.querySelector('.faves')
+        const response = await fetch(addToFaveUrl+pageNum,options);
+        const data = await response.json();
+        data.results.forEach(element => {
+
+            if(autoAddToDomFave(element) != null)
+            {
+              listOfFaves.append(autoAddToDomFave(element));
+            }
+      })
+    }
+}
+
 async function fetchingData()
 {
   heroRemoveFromDom();
@@ -44,7 +67,7 @@ async function fetchingData()
     {
       method: 'GET'
     }
-     
+        let listOfFaves = document.querySelector('.faves')
         let listOfHeros = document.querySelector('.fetched')
         const response = await fetch(url+userInput,options);
         const data = await response.json();
@@ -78,33 +101,90 @@ function NoHeroFound(message)
 }
 
 
+function autoAddToDomFave(hero)
+{
+  let content = document.createElement('li');
+    content.id = "textElement";
+    content.innerText = `Name: ${hero.name} \nHeight: ${hero.height}cm 
+      Birth-Year: ${hero.birth_year} \nGender: ${hero.gender}\n\n`;
+
+  if(hero.name == selectedFaves[0])
+    {
+      autoAddToFave(content.innerText)
+    }
+    else if(hero.name == selectedFaves[1])
+    {
+      autoAddToFave(content.innerText)
+    }
+    else if(hero.name == selectedFaves[2])
+    {
+      autoAddToFave(content.innerText)
+    }
+    else if(hero.name == selectedFaves[3])
+    {
+      autoAddToFave(content.innerText)
+    }
+    else if(hero.name == selectedFaves[4])
+    {
+      autoAddToFave(content.innerText)
+    }
+    else if(hero.name == selectedFaves[5])
+    {
+      autoAddToFave(content.innerText)
+    }
+    else
+    {
+      return null;
+    }
+}
+
+
   function heroToDomAdd(hero)
 {
     let content = document.createElement('li');
     content.id = "textElement";
     content.innerText = `Name: ${hero.name} \nHeight: ${hero.height}cm 
     Birth-Year: ${hero.birth_year} \nGender: ${hero.gender}\n\n`;
+  
     return content;
 }
 
 
+function autoAddToFave(character)
+{
+   
+
+    if(character != null) 
+    {
+      //let value = character.split(" ").join("");
+      if(!AlreadyInFaveList(character))
+      {
+        let el = document.createElement('li');
+        let valueToEl = document.createTextNode(character);
+        el.appendChild(valueToEl);
+        let listOfFaves = document.querySelector('.faves');
+        listOfFaves.appendChild(el);
+  
+      }
+    }
+}
+
 function AddFavorite(pointerValue)
 {
-  let value = pointerValue.split("\n").join("");
+  //let value = pointerValue.split("\n").join("");
   let listOfHeros = document.querySelector('.fetched');
-  if(value != null)
+  if(pointerValue != null)
   {
-    if(!AlreadyInFaveList(value))
+    if(!AlreadyInFaveList(pointerValue))
     {
     
       let el = document.createElement('li');
-      //let newValue = pointerValue.split('').Join(' ');
       let valueToEl = document.createTextNode(pointerValue);
       el.appendChild(valueToEl);
       let listOfFaves = document.querySelector('.faves');
       listOfFaves.appendChild(el);
 
-    }else if(value === "")
+    }else if(pointerValue === "")
     {
       heroRemoveFromDom();
       NoHeroFound("Cant add if there is no search result")
@@ -122,12 +202,19 @@ function AlreadyInFaveList(pointerValue)
   let listOfFaves = document.querySelector('.faves') 
   for(i = 0;i<listOfFaves.childNodes.length;i++)
    {
-    let child = listOfFaves.childNodes[i].innerText.split("\n").join(""); 
-    //let input = userInput.split("\n").join("");
-     if(child === pointerValue)
-     { 
-       return true;
-     }     
+     if(listOfFaves.childNodes[i].innerText != undefined)
+     {
+      let child = listOfFaves.childNodes[i].innerText.split("\n").join("");
+      let splitedChild = child.split(" ").join(""); 
+      let input = pointerValue.split("\n").join("");
+      let splitedInput = input.split(" ").join("")
+
+         if(splitedChild == splitedInput)
+         { 
+          return true;
+         }     
+      }
+  
     }
-   return false;
+    return false;
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# About SWAPI-Web-Scrapper
+Collect starwars character names, birth and gender from https://swapi.dev/ .
+Using async fetch and creates a json object we add to a list in the DOM tree.
+
+
+# Github pages adress to view the project 
+https://carpenteri1.github.io/SWAPI-Web-Scrapper/page.html
